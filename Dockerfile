27928e3b6a68fc4ee82b61a4b23f1203efaaf1cc
jafow <jared.a.fowler@gmail.com>
2020-02-01 22:55:49 -0800

    wip: diff files in pr

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,25 @@
 FROM python:3.8.1-slim-buster
 
+LABEL maintainer="jafow <jared.a.fowler@gmail.com>"
+LABEL "com.github.actions.name"="python-format-pr"
+LABEL "com.github.actions.description"="Format python files in a pull request"
+LABEL "com.github.actions.icon"="activity"
+LABEL "com.github.actions.color"="green"
+
+ENV HUB_VERSION "2.14.1"
+
 COPY LICENSE README.md /
 
-RUN apt-get update -y && apt-get install bash && pip install black
+RUN apt-get update && apt-get install -y bash curl git \
+    && curl -sSL https://github.com/github/hub/releases/download/v$HUB_VERSION/hub-linux-amd64-$HUB_VERSION.tgz | tar -xzpf - \
+    && mv hub-linux-amd64-$HUB_VERSION/bin/hub /bin \
+    && rm -Rf hub-linux-amd64-$HUB_VERSION \
+    && pip install black
 
-COPY entrypoint.sh /app/entrypoint.sh
 
-WORKDIR /app
 
-ENTRYPOINT ["/app/entrypoint.sh"]
+COPY entrypoint.sh /entrypoint.sh
 
-# CMD ["/bin/bash"]
+RUN chmod u+x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,55 @@
 #!/usr/bin/env bash
+set -e;
+set -u pipefail;
 
-input_path=$1
+################################################################################
+# Variables
+#
+# all variables prefixed with "GITHUB_" are passed in via the action context
+################################################################################
+API_VERSION=v3
+BASE_URL=https://api.github.com
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+HEADERS="Accept: application/vnd.github.${API_VERSION}+json"
 
-if [[ -z "${input_path}" ]]; then
-    input_path=`pwd`
-fi
+## url
+REPO="${BASE_URL}/repos/${GITHUB_REPOSITORY}"
+PULL_REQUESTS="${REPO}/pulls"
 
-echo "args: looking at $input_path"
+## repo
+HEAD="${GITHUB_REF}" # ref to the branch that triggered the pull request
+BASE=$(jq --raw-output .pull_request.base.sha "${GITHUB_EVENT_PATH}")
 
-fileslist=$(find $input_path -name "*.py" -type "f")
+printf "%s\n" "head is ${HEAD}"
+printf "%s\n" "BASE is ${BASE}"
 
-# run black over the files list
-black $fileslist
+git fetch origin "${BASE}"
+
+fileslist=$(git diff origin "${BASE}" --name-only | tr ' ' '\n' | grep -E '.py$')
+
+##
+# get all the files in the PR
+# filter the python files (if any)
+# format the files
+# if a diff exists
+#   cut a branch "format/$ORIG_BRANCH"
+#   add / commit
+#   push
+#   check no PRS open for current fix branch
+#   create a pr to ORIG_BRANCH
+# prosper
+
+# input_path=$1
+
+# if [[ -z "${input_path}" ]]; then
+#     input_path=`pwd`
+# fi
+
+# echo "args: looking at $input_path"
+
+# fileslist=$(find $input_path -name "*.py" -type "f")
+
+# # run black over the files list
+# black $fileslist
 
 echo ::set-output name=fileslist::$fileslist
