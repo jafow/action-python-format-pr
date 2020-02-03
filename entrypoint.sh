faff477590fc91247ab55bb18d323c62f0d8ba55
jafow <jared.a.fowler@gmail.com>
2020-02-02 21:09:56 -0800

    cleanup and doc

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,7 @@ set -o pipefail;
 #
 # all variables prefixed with "GITHUB_" are passed in via the action context
 ################################################################################
-## repo
-HEAD=$(jq --raw-output .pull_request.head.ref "${GITHUB_EVENT_PATH}") # ref to the branch that triggered the pull request
+HEAD=$(jq --raw-output .pull_request.head.ref "${GITHUB_EVENT_PATH}")
 BASE=$(jq --raw-output .pull_request.base.sha "${GITHUB_EVENT_PATH}")
 REMOTE=origin
 
@@ -37,46 +36,57 @@ clear_remote_branch() {
     fi
 }
 
+configure_git_client() {
+    # set git client running in ci environment using the provided GITHUB_TOKEN
+    # in the remote url
+    git config --global user.email "formatbot@no-reply.com"
+    git config --global user.name "FormatBot"
 
-################################################################################
-# configure the git client
-################################################################################
-git config --global user.email "formatbot@boop.net"
-git config --global user.name "FormatBot"
+    # set the url of REMOTE to include the PAT token to auth to the repo. 
+    git remote set-url "${REMOTE}" "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+    git fetch "${REMOTE}" "${BASE}"
+}
 
-git remote rm "${REMOTE}"
-git remote add "${REMOTE}" "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git fetch "${REMOTE}" "${BASE}"
+main() {
+    configure_git_client
 
-formattable=$(git diff "${BASE}" --name-only | tr ' ' '\n' | grep -E '.py$')
-printf "%s\n" "getting files list ${formattable}"
+    # check diff has python files 
+    formattable=$(git diff "${BASE}" --name-only | tr ' ' '\n' | grep -E '.py$')
+    if [[ -z "${formattable}" ]]; then
+        printf "%s\n" "No *.py files found to format. Nothing to do.\n"
+        echo ::set-output name=fileslist::$formattable
+        exit 0
+    fi
 
-black -q -l 120 $formattable
+    # There are *.py files; Run formatter.
+    # TODO allow the formatter and options to be passed inputs
+    black -q -l 120 $formattable
 
-# if no files in diff then everything is already formatted
-is_already_formatted=$(git diff --name-only)
+    # if no files in diff then everything is already formatted
+    is_already_formatted=$(git diff --name-only)
 
-if [[ -z "${is_already_formatted}" ]]; then
-    printf "nothing to format. Exiting.\n"
-    exit 0
-fi
+    if [[ -z "${is_already_formatted}" ]]; then
+        printf "nothing to format. Exiting.\n"
+        exit 0
+    fi
 
-# otherwise we cut a branch and add + commit the changes
-FORMAT_BRANCH="format/${HEAD}"
+    # otherwise we cut a branch and add + commit the changes
+    FORMAT_BRANCH="format/${HEAD}"
 
-# delete any local copy of the branch
-clear_local_branch "${FORMAT_BRANCH}"|| handle_delete_missing_branch "${FORMAT_BRANCH}"
-clear_remote_branch "${FORMAT_BRANCH}" || handle_delete_missing_branch "${FORMAT_BRANCH}"
+    # delete any local copy of the branch
+    clear_local_branch "${FORMAT_BRANCH}"|| handle_delete_missing_branch "${FORMAT_BRANCH}"
+    clear_remote_branch "${FORMAT_BRANCH}" || handle_delete_missing_branch "${FORMAT_BRANCH}"
 
-git checkout -b "${FORMAT_BRANCH}"
+    git checkout -b "${FORMAT_BRANCH}"
 
-# add the formatted files, commit them, and push the branch
-git add .
-git commit -m "python-format-action: run black over PR #$(jq -r .pull_request.number $GITHUB_EVENT_PATH)"
-git push "${REMOTE}" "${FORMAT_BRANCH}"
+    # add the formatted files, commit them, and push the branch
+    git add .
+    git commit -m "python-format-action: run black over PR #$(jq -r .pull_request.number $GITHUB_EVENT_PATH)"
+    git push "${REMOTE}" "${FORMAT_BRANCH}"
 
-printf "opening PR\n"
-hub pull-request -b "${HEAD}" -h "${FORMAT_BRANCH}" -a "${GITHUB_ACTOR}" -m "python-format-action: fixing files that need formatting"
+    printf "opening PR\n"
+    hub pull-request -b "${HEAD}" -h "${FORMAT_BRANCH}" -a "${GITHUB_ACTOR}" -m "python-format-action: fixing files that need formatting"
 
-# output the list of formatted files
-echo ::set-output name=fileslist::$formattable
+    # output the list of formatted files
+    echo ::set-output name=fileslist::$formattable
+}
